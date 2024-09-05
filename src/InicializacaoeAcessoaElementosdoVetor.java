5a8ba4b02a2932788b4cb824405edef613cf2b93
ruanbauerr <ruanbauer4@gmail.com>
2024-09-05 07:33:43 -0300

    Atividade 1 feita

---
+++ src/InicializacaoeAcessoaElementosdoVetor.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class InicializacaoeAcessoaElementosdoVetor {
+    public static void main(String[] args) {
+
+        int[] numero = new int[5];
+        numero[0] = 10;
+        numero[1] = 20;
+        numero[2] = 30;
+        numero[3] = 40;
+        numero[4] = 50;
+        System.out.println("O terceiro elemento é: " + numero[2]);
+
+
+    }
+}
